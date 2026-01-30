70d159221ef537bb08df829d30c61f3fd93eaaef
Liana Pavlicheva <liana_p@icloud.com>
2026-01-30 15:16:09 +1100

    .

--- send_form.php
+++ send_form.php
@@ -6,17 +6,33 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 }
 
 // Helper to safely get POST values
-function post($key){
+function post_value($key) {
     return isset($_POST[$key]) ? trim($_POST[$key]) : '';
 }
 
-$name = strip_tags(post('name'));
-$business = strip_tags(post('business'));
-$website = strip_tags(post('website'));
-$email = filter_var(post('email'), FILTER_SANITIZE_EMAIL);
-$note = strip_tags(post('note'));
+function strip_newlines($value) {
+    return str_replace(["\r", "\n"], '', $value);
+}
 
-if (!$name || !$email || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+$name = strip_tags(post_value('name'));
+$business = strip_tags(post_value('business'));
+$website = strip_tags(post_value('website'));
+$email = filter_var(post_value('email'), FILTER_SANITIZE_EMAIL);
+$note = strip_tags(post_value('note'));
+
+$name = strip_newlines($name);
+$business = strip_newlines($business);
+$website = strip_newlines($website);
+$email = strip_newlines($email);
+
+if (
+    !$name ||
+    !$business ||
+    !$website ||
+    !$email ||
+    !filter_var($email, FILTER_VALIDATE_EMAIL) ||
+    !filter_var($website, FILTER_VALIDATE_URL)
+) {
     header('Location: index.html?success=0');
     exit;
 }
@@ -30,18 +46,17 @@ $body .= "Website: {$website}\n";
 $body .= "Email: {$email}\n\n";
 $body .= "Note:\n{$note}\n";
 
+$host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'webgirlstudio.com';
+$from_domain = preg_replace('/[^a-z0-9.-]/i', '', $host);
+$from_address = 'no-reply@' . $from_domain;
+
 $headers = [];
-$headers[] = 'From: Web Girl Studio <no-reply@webgirlstudio.local>';
+$headers[] = 'From: Web Girl Studio <' . $from_address . '>';
 $headers[] = 'Reply-To: ' . $email;
 $headers[] = 'Content-Type: text/plain; charset=UTF-8';
 
 $sent = mail($to, $subject, $body, implode("\r\n", $headers));
 
-if ($sent) {
-    header('Location: index.html?success=1');
-} else {
-    header('Location: index.html?success=0');
-}
+header('Location: index.html?success=' . ($sent ? '1' : '0'));
 exit;
-
 ?>
